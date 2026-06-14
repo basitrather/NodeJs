db9f3b1513d5fc084704d17c2ce7f0718734cbdd
BasitRather <ratherbasit253@gmail.com>
2026-06-14 21:44:05 +0530

    Mini Project Node Farm completed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Using Node.js core modules
 const fs = require('fs'); //Importing the FileSystem module
 const http = require('http');
+const path = require('path');
 const url = require('url');
 
 ////////////////////////
@@ -60,4 +61,66 @@ const routingServer = http.createServer((req, res) => {
 //Now listening to the server or startint it
 routingServer.listen(6000, '127.0.0.1', () => {
   console.log('Hello the server started on localHost');
+});
+
+// Mini Project Node-Farm
+const replaceTemplate = function (template, product) {
+  let output = template.replace(/{%NAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+  if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+  return output;
+};
+const templateOverview = fs.readFileSync('./templates/template-overview.html', 'utf-8');
+const templateCard = fs.readFileSync('./templates/template-card.html', 'utf-8');
+const templateProduct = fs.readFileSync('./templates/template-product.html', 'utf-8');
+const data = fs.readFileSync('./dev-data/data.json', 'utf-8');
+const dataObj = JSON.parse(data);
+
+const nodeFarmServer = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
+
+  //Overview Page
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    const cardsHTML = dataObj.map((ele) => replaceTemplate(templateCard, ele)).join('');
+    const output = templateOverview.replace('{%CARD%}', cardsHTML);
+    res.end(output);
+  }
+
+  //Product Page
+  else if (pathname === '/product') {
+    const product = dataObj[query.id];
+    if (!product) {
+      res.writeHead(404, { 'Content-Type': 'text/html' });
+      return res.end('<h1>Product not found</h1>');
+    }
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    const output = replaceTemplate(templateProduct, product);
+    res.end(output);
+  }
+
+  //API Page
+  else if (pathname === '/api') {
+    res.writeHead(200, { 'Content-type': 'application/json' });
+    res.end(data);
+  }
+
+  //Page not found
+  else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'Hello world',
+    });
+    res.end(`<h1>Page not found</h1>`);
+  }
+});
+
+nodeFarmServer.listen(5000, '127.0.0.1', () => {
+  console.log('Hello the Node farm server started.');
 });
